3ae62dc4ae6e8d60ccd305ce08f03196d36cf792
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-26 23:32:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,14 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
      season_caps.join(" ")
      puts season_caps[0] + ":"
     
-     binding.pry
-     holiday_hash[season].each do |holiday|
-       supplies = holiday.values
-       supplies.collect {|word| word.capitalize}
-       supplies.join(", ")
-       
-       
-     end
+    
      
    end
  end
